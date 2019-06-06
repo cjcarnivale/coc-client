339894d92fde3404fb3f947cf65502176bbf43b2
Christopher <christopher.carnivale@gmail.com>
2019-06-06 19:17:05 -0400

    currency populates from database

--- src/SafeCountCalculator/SafeCountCalculator.js
+++ src/SafeCountCalculator/SafeCountCalculator.js
@@ -1,62 +1,14 @@
 import React, { Component } from "react";
 import "./SafeCountCalculator.css";
+import Config from "../config";
 
 export default class SafeCountCalculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currency: [
-        {
-          name: "Quarters",
-          count: 0,
-          multiplier: 10
-        },
-        {
-          name: "Dimes",
-          count: 0,
-          multiplier: 5
-        },
-        {
-          name: "Nickles",
-          count: 0,
-          multiplier: 2
-        },
-        {
-          name: "Pennies",
-          count: 0,
-          multiplier: 0.5
-        },
-        {
-          name: "Ones",
-          count: 0,
-          multiplier: 1
-        },
-        {
-          name: "Fives",
-          count: 0,
-          multiplier: 5
-        },
-        {
-          name: "Tens",
-          count: 0,
-          multiplier: 10
-        },
-        {
-          name: "Twenties",
-          count: 0,
-          multiplier: 20
-        },
-        {
-          name: "Fifties",
-          count: 0,
-          multiplier: 50
-        },
-        {
-          name: "Hundreds",
-          count: 0,
-          multiplier: 100
-        }
-      ]
+      currency: [],
+      isLoaded: false,
+      error: null
     };
   }
 
@@ -71,31 +23,54 @@ export default class SafeCountCalculator extends Component {
   render() {
     return (
       <div className="count-form-container">
-        <form className="count-form">
-          {this.state.currency.map((den, i) => (
-            <div className="currency-item" key={i}>
-              <span>{den.name}</span>
-              <input
-                type="number"
-                min="0"
-                value={this.state.currency[i].count}
-                onChange={e => this.updateCount(i, e)}
-              />
-              <span>{i < 4 ? "roll(s)" : "bill(s)"}</span>
-              <span>Total: $ {den.count * den.multiplier}</span>
-            </div>
-          ))}
-          <div className="grand-total">
-            Grand Total:{" "}
-            {this.state.currency.reduce(
-              (accumulator, currentValue) =>
-                accumulator + currentValue.count * currentValue.multiplier,
-              0
-            )}
+        {!this.state.isLoaded 
+          ? <div>Loading</div> 
+          : this.state.error 
+            ? <div>Error: {this.state.error.message}</div>
+            : <form className="count-form">
+              {this.state.currency.map((den, i) => (
+                <div className="currency-item" key={i}>
+                  <span>{den.name}</span>
+                  <input
+                    type="number"
+                    min="0"
+                    value={this.state.currency[i].count}
+                    onChange={e => this.updateCount(i, e)}
+                  />
+                  <span>{i < 4 ? "roll(s)" : "bill(s)"}</span>
+                  <span>Total: $ {den.count * den.multiplier}</span>
+                </div>
+              ))}
+              <div className="grand-total">
+                Grand Total:{" "}
+                {this.state.currency.reduce(
+                  (accumulator, currentValue) =>
+                    accumulator + currentValue.count * currentValue.multiplier,
+                  0
+                )}
+              </div>
+              <button type="submit">Submit Count</button>
+            </form>}
           </div>
-            <button type="submit">Submit Count</button>
-        </form>
-      </div>
     );
+  }
+
+  componentDidMount() {
+    fetch(`${Config.API_ENDPOINT}/denominations`)
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            currency: result
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
   }
 }
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  API_ENDPOINT: 'http://localhost:8000/api',
+}
+
+//process.env.REACT_APP_API_BASE 
