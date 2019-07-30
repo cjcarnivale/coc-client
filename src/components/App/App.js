fa86a172229816b2a211fb615bf3df3acf9601a1
Christopher <christopher.carnivale@gmail.com>
2019-07-30 17:49:03 -0400

    router logic in place

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom"; 
 import Dashboard from "../../routes/Dashboard/Dashboard.Route";
 class App extends Component {
   render() {
     return (
       <main className="App">
-        <Dashboard />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Dashboard}/>
+          </Switch>
+        </BrowserRouter>
       </main>
-    );
+    ); 
   }
 }
 
